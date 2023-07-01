dde70070f5f0fce857908e80d2a4b01ce4964537
99Raul <rauljr96@gmail.com>
2023-06-30 18:47:46 -0700

    added logic changes to tile calculation/ui

--- components/TileCalc.tsx
+++ components/TileCalc.tsx
@@ -18,11 +18,16 @@ import {
 	HoverCardTrigger,
 } from '@/components/ui/hover-card';
 import { InformationCircleIcon } from '@heroicons/react/20/solid';
+// import { isNull } from 'util';
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
+import { Terminal, Waves } from 'lucide-react';
 
 const TileCalculator: React.FC = () => {
-	const [length, setLength] = useState<number>(0);
-	const [width, setWidth] = useState<number>(0);
-	const [diameter, setDiameter] = useState<number>(0);
+	const [length, setLength] = useState<number | null>(null);
+	const [width, setWidth] = useState<number | null>(null);
+	// const [length, setLength] = useState<number>(0);
+	// const [width, setWidth] = useState<number>(0);
+	const [diameter, setDiameter] = useState<number | null>(null);
 	const [isInches, setIsInches] = useState<boolean>(true);
 	const [isInches2, setIsInches2] = useState<boolean>(true);
 	const [coverage, setCoverage] = useState<number>(0);
@@ -36,22 +41,59 @@ const TileCalculator: React.FC = () => {
 	const [customTileCost, setCustomTileCost] = useState<number>(0);
 	const [totalCost, setTotalCost] = useState<number>(0);
 
-	const calculateSquareCoverage = () => {
-		const totalCoverage = (
-			(isInches ? length * width : length * width * 144) / 144
-		).toFixed(2);
-		setCoverage(parseFloat(totalCoverage));
+	// for alert display
+	const [isCalculated, setIsCalculated] = useState<boolean>(false);
+
+	//had to do it this way to enter decimal values
+	const handleChangeLength = (e: ChangeEvent<HTMLInputElement>) => {
+		const value = parseInt(e.target.value);
+		setLength(isNaN(value) ? null : value);
 	};
 
+	const handleChangeWidth = (e: ChangeEvent<HTMLInputElement>) => {
+		const value = parseInt(e.target.value);
+		setWidth(isNaN(value) ? null : value);
+	};
+
+	const handleChangeDiameter = (e: ChangeEvent<HTMLInputElement>) => {
+		const value = parseInt(e.target.value);
+		setDiameter(isNaN(value) ? null : value);
+	};
+
+	// const calculateSquareCoverage = () => {
+	// 	const totalCoverage = (
+	// 		(isInches ? length * width : length * width * 144) / 144
+	// 	).toFixed(2);
+	// 	setCoverage(parseFloat(totalCoverage));
+	// };
+
+	const calculateSquareCoverage = () => {
+		if (length !== null && width !== null) {
+			const totalCoverage =
+				((isInches ? length : length * 12) * (isInches ? width : width * 12)) /
+				144;
+			setCoverage(parseFloat(totalCoverage.toFixed(2)));
+		}
+	};
 	const calculateRoundCoverage2 = () => {
-		const radius = isInches2 ? diameter / 2 : (diameter / 2) * 12;
-		const totalCoverage = Math.ceil(radius * radius * Math.PI);
-		setCircleCoverage(totalCoverage);
+		if (diameter !== null) {
+			const radius = isInches2 ? diameter / 2 : (diameter / 2) * 12;
+			const totalCoverage = Math.ceil(radius * radius * Math.PI);
+			setCircleCoverage(totalCoverage);
+		}
 	};
 
 	const calculateTileCount = () => {
+		if (coverage === 0 || tileArea === 0) {
+			// Display an error or show a message indicating that the coverage and tile area must be calculated first
+			setIsCalculated(false); // Set isCalculated to false
+			console.log('Please calculate coverage and tile area first.');
+			return;
+		}
+
 		const tilesNeeded = Math.ceil(coverage / tileArea);
 		setTileCount(tilesNeeded);
+		setIsCalculated(true); // Set isCalculated to true
 	};
 
 	const calculateTotalTiles = () => {
@@ -81,9 +123,9 @@ const TileCalculator: React.FC = () => {
 	};
 
 	const resetCalculator = () => {
-		setLength(0);
-		setWidth(0);
-		setDiameter(0);
+		setLength(null);
+		setWidth(null);
+		setDiameter(null);
 		setIsInches(true);
 		setIsInches2(true);
 		setCoverage(0);
@@ -130,8 +172,9 @@ const TileCalculator: React.FC = () => {
 						<Label htmlFor='name'>Length</Label>
 						<Input
 							type='number'
-							value={length}
-							onChange={(e) => setLength(parseInt(e.target.value))}
+							step='any'
+							value={length !== null ? length : ''}
+							onChange={handleChangeLength}
 						/>
 					</div>
 					<div>
@@ -139,8 +182,9 @@ const TileCalculator: React.FC = () => {
 							<Label htmlFor='name'>Width</Label>
 							<Input
 								type='number'
-								value={width}
-								onChange={(e) => setWidth(parseInt(e.target.value))}
+								step='any'
+								value={width !== null ? width : ''}
+								onChange={handleChangeWidth}
 							/>
 						</div>
 					</div>
@@ -181,22 +225,36 @@ const TileCalculator: React.FC = () => {
 											</HoverCardContent>
 										</HoverCard>
 									</div>
+									{!isCalculated && (
+										<Alert variant={'destructive'}>
+											<AlertTitle>Heads up!</AlertTitle>
+											<AlertDescription>
+												Please enter tile area first, do not leave blank or 0
+											</AlertDescription>
+										</Alert>
+									)}
 
 									<Input
 										type='number'
+										step='any'
 										value={tileArea}
 										onChange={(e) => setTileArea(parseInt(e.target.value))}
 									/>
 								</div>
 							</div>
-							<Button onClick={calculateTileCount}>Calculate Tile Count</Button>
-
+							{/*// Render the tile count calculation button conditionally */}
+							{coverage > 0 && tileArea > 0 && (
+								<Button onClick={calculateTileCount}>
+									Calculate Tile Count
+								</Button>
+							)}
 							{tileCount > 0 && (
 								<>
 									<div className='flex flex-col space-y-1.5'>
 										<Label>Overage/Material Waste (%):</Label>
 										<Input
 											type='number'
+											step='any'
 											value={overage}
 											onChange={(e) => setOverage(parseInt(e.target.value))}
 										/>
@@ -218,17 +276,10 @@ const TileCalculator: React.FC = () => {
 							{totalTiles > 0 && (
 								<>
 									<div className='flex flex-col space-y-3'>
-										{/* <Label htmlFor='totalTiles'>Total Tiles:</Label>
-										<Input
-											type='number'
-											id='totalTiles'
-											value={totalTiles}
-											onChange={handleTotalTilesChange}
-										/> */}
-
 										<Label htmlFor='customTileCost'>Custom Tile Cost $:</Label>
 										<Input
 											type='number'
+											step='any'
 											id='customTileCost'
 											value={customTileCost}
 											onChange={handleCustomTileCostChange}
@@ -273,8 +324,9 @@ const TileCalculator: React.FC = () => {
 						<Label>Diameter:</Label>
 						<Input
 							type='number'
-							value={diameter}
-							onChange={(e) => setDiameter(parseInt(e.target.value))}
+							step='any'
+							value={diameter !== null ? diameter : ''}
+							onChange={handleChangeDiameter}
 						/>
 					</div>
 					<Button onClick={calculateRoundCoverage2}>Calculate Coverage</Button>
