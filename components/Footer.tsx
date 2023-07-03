8986fec2df9cf08d43ba81fba374a341c421f73a
99Raul <rauljr96@gmail.com>
2023-07-03 11:20:44 -0700

    added footer link

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,18 +1,21 @@
-
+import { Github } from 'lucide-react';
+import Link from 'next/link';
 
 export default function Footer() {
 	return (
-		<div className='absolute w-full border-t py-5 text-center'>
-			<p className=''>
-				Footer template{' '}
-				<a
-					className='font-medium  underline transition-colors'
-					href=''
-					target='_blank'
-					rel='noopener noreferrer'
-				>
-					Raul
-				</a>
+		<div className='flex flex-row w-full border-t py-5 text-center justify-center'>
+			<p className='flex'>
+				Github
+				<div className='flex ml-2'>
+					<Link
+						className='flex flex-row justify-center'
+						href='https://github.com/99Raul/construction_hub'
+						target='_blank'
+						rel='noopener noreferrer'
+					>
+						<Github />
+					</Link>
+				</div>
 			</p>
 		</div>
 	);
